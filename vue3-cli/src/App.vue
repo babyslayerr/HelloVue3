c86fd8c58aeb7b50cbc3c41b0304995e9b4ae566
JongHwanJeong <96283391+babyslayerr@users.noreply.github.com>
2024-08-04 19:36:51 +0900

    Vue cli를 이용한 프로젝트(vue3-cli 폴더)와 npx를 이용한 프로젝트(vue3 폴더) 생성, 별도 html안의 CDN 환경에서 컴포넌트 등록

---
+++ vue3-cli/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png"> <!-- 더이상 Vue3에서는 root태그가 필요없어졌음 -->
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue' // HelloWorld Vue컴포넌트 파일을 import
+
+export default { // Vue의 인스턴스 옵션 속성 후에 다른 컴포넌트(루트컴포넌트 포함)에서 사용하기 위함 -> 후에 main.js에 의해 mount될 dom영역에 최종적으로 사용됨(마지막 create(App<-여기에 import하기 위함)
+  name: 'App', // 후에 main.js에서 create(App) 에서 속성이 import됨
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue3-project/src/main.js
@@ -0,0 +1,6 @@
+import './assets/main.css'
+
+import { createApp } from 'vue' // package.json 에 선언되어있는 라이브러리 명을 가져와서 createApp함수를 사용
+import App from './App.vue' //  .vue파일안의
+
+createApp(App).mount('#app') //
